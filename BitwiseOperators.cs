30979543ec7e3c484cde5910c7b24679bbb3992a
priyanshu <priyanshu.sharma_cs.ccv21@gla.ac.in>
2025-01-20 20:12:42 +0530

    bitwise operators completed

---
+++ BitwiseOperators.cs
@@ -0,0 +1,15 @@
+﻿﻿using System;
+
+class BitwiseOperators {
+static void Main(string[] args) {
+int a = Convert.ToInt32(Console.ReadLine()); 
+int b = Convert.ToInt32(Console.ReadLine());  
+
+Console.WriteLine("a & b: " + (a & b)); 
+Console.WriteLine("a | b: " + (a | b));
+Console.WriteLine("a ^ b: " + (a ^ b)); 
+Console.WriteLine("~a: " + (~a)); 
+Console.WriteLine("a << 1: " + (a << 1)); 
+Console.WriteLine("a >> 1: " + (a >> 1)); 
+}
+}
