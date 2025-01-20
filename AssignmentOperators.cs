a082bdb8304bbf581889b486316bfe23919afb38
priyanshu <priyanshu.sharma_cs.ccv21@gla.ac.in>
2025-01-20 16:54:41 +0530

    assignment operators completed

---
+++ AssignmentOperators.cs
@@ -0,0 +1,23 @@
+﻿﻿using System;
+
+class AssignmentOperators {
+static void Main(string[] args) {
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+a += b;
+Console.WriteLine("a += b: " + a); 
+
+a -= b; 
+Console.WriteLine("a -= b: " + a); 
+
+a *= b; 
+Console.WriteLine("a *= b: " + a); 
+
+a /= b; 
+Console.WriteLine("a /= b: " + a); 
+
+a %= b;
+Console.WriteLine("a %= b: " + a); 
+}
+}
